af36327d56eeea51d2e5623e2e9a3647e54c89c3
ArtemReva <artemreva79@gmail.com>
2025-04-11 11:37:22 +0300

    update moduleData with detailed content for submodules

--- client/src/lib/moduleData.ts
+++ client/src/lib/moduleData.ts
@@ -9,11 +9,13 @@ export const modules: Module[] = [
     submodules: [
       {
         title: "Self-awareness: Understand your engagement style",
-        content: "Lorem ipsum",
+        content:
+          "<p><b>Ever wonder why you instantly click with some people yet butt heads with others?</b> Turns out there's a scientific reason...</p>",
       },
       {
         title: "Other awareness: Engage effectively with your team",
-        content: "Lorem ipsum",
+        content:
+          "<p><b>People don't come with a user manual</b> but wouldn't it be great if they did?...</p>",
       },
     ],
   },
@@ -23,8 +25,21 @@ export const modules: Module[] = [
     summary:
       "Master techniques for building rapport, setting expectations, and creating psychological safety in your team.",
     submodules: [
-      { title: "Lorem Ipsum", content: "Lorem Ipsum" },
-      { title: "Lorem Ipsum", content: "Lorem Ipsum" },
+      {
+        title: "Engage fully with your teammates",
+        content:
+          "<p><b>How would you feel if you and I were having a conversation at work</b> and I kept glancing at my phone?...</p>",
+      },
+      {
+        title: "Manage expectations and foster trust",
+        content:
+          "<p><b>In any relationship, at work or otherwise,</b> usually the catalyst to friction is unfulfilled expectations...</p>",
+      },
+      {
+        title: "Foster psychological safety",
+        content:
+          "<p><b>Google conducted a major internal study</b> to identify the key dynamics of successful teams...</p>",
+      },
     ],
   },
   {
@@ -33,8 +48,21 @@ export const modules: Module[] = [
     summary:
       "Learn to navigate conflict, offer constructive feedback, and foster a collaborative team environment.",
     submodules: [
-      { title: "Lorem Ipsum", content: "Lorem Ipsum" },
-      { title: "Lorem Ipsum", content: "Lorem Ipsum" },
+      {
+        title: "Capitalize on collaboration for team building",
+        content:
+          "<p><b>Collaboration is at the heart of teamwork.</b> And when done right, the team adds up to much more than the sum of its parts...</p>",
+      },
+      {
+        title: "Confidently navigate conflict and offer feedback",
+        content:
+          "<p><b>The more diverse the team, the more variation in perspectives</b>, and inevitably the more potential for conflict...</p>",
+      },
+      {
+        title: "Play to your strengths",
+        content:
+          "<p><b>A decade ago, when someone would ask, what are your strengths?</b> I'd reply with all the things I was good at doing...</p>",
+      },
     ],
   },
   {
@@ -43,8 +71,16 @@ export const modules: Module[] = [
     summary:
       "Develop strategic thinking abilities, leverage your strengths, and adopt a solution-focused approach to challenges.",
     submodules: [
-      { title: "Lorem Ipsum", content: "Lorem Ipsum" },
-      { title: "Lorem Ipsum", content: "Lorem Ipsum" },
+      {
+        title: "Hone your strategy skills",
+        content:
+          "<p><b>High performing teams need team members</b> who are able to think strategically in order to be proactive...</p>",
+      },
+      {
+        title: "Develop your solution-orientation",
+        content:
+          "<p><b>How you show up when you're with your team</b> matters more than you realize...</p>",
+      },
     ],
   },
 ];
